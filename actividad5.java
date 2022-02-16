c60b1d29319cb1b035b77027d80faa44451109a4
Arturo Manrique <arturomgzm@hotmail.com>
2022-02-16 12:03:32 -0600

    programa

---
+++ actividad5.java
@@ -0,0 +1,24 @@
+/*
+    Elaborado por Arturo Manrique Garza
+    Al03009720
+*/
+public class actividad5 {
+    public static void main(String[] args) {
+        //se utilizaran doubles para poder aceptar decimales
+        double x = 5;
+        double y = 2;
+        //impresion de resultados
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+        //la suma utiliza el operador +
+        System.out.println("Suma: " + (x+y));
+        //la resta utiliza el operador -
+        System.out.println("Resta: " + (x-y));
+        //La multiplicación utiliza el operador *
+        System.out.println("Multiplicación: " + (x*y));
+        //la división utiliza el operador /
+        System.out.println("División: " + (x/y));
+        //El módulo utiliza el operador %
+        System.out.println("Módulo: " + (x%y));
+    }
+}
